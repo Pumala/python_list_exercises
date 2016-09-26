e1f569fd31f34a1271290dca8fd1a3bc6dbb8911
Carolyn Lam <caro_lyn_@hotmail.com>
2016-09-26 00:04:27 -0400

    add README.md file

---
+++ README.md
@@ -0,0 +1,3 @@
+* Python Loop Exercises
+
+* Solve different logic challenges in python using lists
---
+++ de_dup.py
@@ -0,0 +1,6 @@
+print "De-dup Exercise"
+# Given an list of numbers or strings, create a new list containing the
+# same elements as the first list, except with any duplicate values removed.
+# Print the list.
+
+num_list = [1, 4, 5, 4, 3, 1, 1, 7, 8, 5, 9, 5]
