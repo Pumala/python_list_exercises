cbe9aa17dc681eee418c7ba70f510a39a081ad9d
Carolyn Lam <caro_lyn_@hotmail.com>
2016-09-26 22:19:33 -0400

    update de_dup.py logic

--- de_dup.py
+++ de_dup.py
@@ -1,81 +1,13 @@
-# print "De-dup Exercise"
+print "De-dup Exercise"
 # Given a list of numbers or strings, create a new list containing the
 # same elements as the first list, except with any duplicate values removed.
 # Print the list.
 
-num_list = [1, 4, 5, 4, 3, 1, 1, 7, 8, 5, 9, 5]
-num_list_new = [1, 4, 5, 4, 3, 1, 1, 7, 8, 5, 9, 5]
-num_list2 = []
+mixed_list = [1, "cat", 4, "dog", 8, "cat", 5, 8, "fish", 4, "monkey", 5, "tuna", "dog", "fish", "dog", 5]
+no_dups_list = []
 
-for num1 in num_list_new:
-    counter = 0
-    num_list2.append(num1)
-    for num2 in num_list2:
-        if num1 == num_list2[counter]:
-            # del num_list[counter]
-            del num_list2[num1]
-            counter -= 1
-            # deleted += 1
-        counter += 1
-    if counter == len(num_list):
-        counter = 0
+for element in mixed_list:
+    if element not in no_dups_list:
+        no_dups_list.append(element)
 
-print num_list2
-
-
-# num_list = [1, 4, 5, 4, 3, 1, 1, 7, 8, 5, 9, 5]
-# num_list_new = [1, 4, 5, 4, 3, 1, 1, 7, 8, 5, 9, 5]
-# num_list2 = []
-#
-# for num1 in num_list_new:
-#     counter = 0
-#     num_list2.append(num1)
-#     for num2 in num_list:
-#         if num1 == num_list[counter]:
-#             del num_list[counter]
-#             counter -= 1
-#             # deleted += 1
-#         counter += 1
-#     if counter == len(num_list):
-#         counter = 0
-#
-# print num_list2
-
-
-# for num1 in num_list:
-#     counter = 0
-#     deleted = 0
-#     index = num_list.index[num1]
-#     num_list2.append(num_list[(index - deleted)])
-#     for num2 in num_list:
-#         if num1 == num_list[counter]:
-#             del num_list[counter]
-#             counter -= 1
-#             deleted += 1
-#         counter += 1
-#     if counter == len(num_list):
-#         counter = 0
-#
-# print num_list2
-
-
-# for num1 in num_list:
-#     counter = 0
-#     num_list2.append(num1)
-#     # match = False
-#     for num2 in num_list:
-#         # compare_me = num1
-#         # num_list2.append(num1)
-#         # counter = 1
-#         # if num1 == num_list[counter + 1]:
-#         if num1 == num_list[counter]:
-#         # if num1 == num2 and match == True:
-#             # del num_list[counter + 1]
-#             del num_list[counter + 1]
-#             # del num_list2[counter]
-#         match = True
-#     counter += 1
-#     if counter == len(num_list):
-#         counter = 0
-#
-# print num_list2
+print no_dups_list
